307a92cce1a330710dcbbfe9f275b2cb761b93ce
trzynasty_ <goniwiechamaja123@gmail.com>
2025-10-07 09:32:27 +0200

    do dupy

--- app/src/main/java/com/example/quiz/MainActivity.java
+++ app/src/main/java/com/example/quiz/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.quiz;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +13,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity {
+    private ArrayList<Pytanie> pytania = new ArrayList<>();
+    private Button bTak, bNie, bPodpowiedz, bNext;
+    private TextView trescPytania;
+    private ImageView zdjecie;
+    private int numerPytanka = 0, iloscPytan = 3, wynik = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +31,70 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        pytania.add(new Pytanie(R.drawable.wiewiora, "Czy wiewiórki zapadają w sen zimowy?", "Wiewiórki robią zapasy na zimę", true));
+        pytania.add(new Pytanie(R.drawable.bociek, "huh??", "nwm", true));
+        pytania.add(new Pytanie(R.drawable.krasnal, "z krakowa??", "nie", false));
+
+        bNie = findViewById(R.id.button3);
+        bTak = findViewById(R.id.button4);
+        bNext = findViewById(R.id.button6);
+        trescPytania = findViewById(R.id.trescPytanie);
+        zdjecie = findViewById(R.id.obrazek1);
+        wyswietlPytanie(numerPytanka);
+
+        bNext.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if(numerPytanka < pytania.size()) {
+                            numerPytanka++;
+                            wyswietlPytanie(numerPytanka);
+                        }
+//
+
+                    }
+                }
+        );
+
+        bTak.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        sprawdzeniePoprawności(numerPytanka, true);
+                        numerPytanka++;
+                        wyswietlPytanie(numerPytanka);
+                    }
+                }
+        );
+
+        bNie.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        sprawdzeniePoprawności(numerPytanka, false);
+                        numerPytanka++;
+                        wyswietlPytanie(numerPytanka);
+                    }
+                }
+        );
+
+
     }
+
+    private void wyswietlPytanie(int ktorePytanie){
+        trescPytania.setText(pytania.get(ktorePytanie).getTrescPytania());
+        zdjecie.setImageResource(pytania.get(ktorePytanie).getIdObrazek());
+    }
+    private void sprawdzeniePoprawności(int ktorePytanie, boolean udzielonaOdpowniedz){
+        if(pytania.get(ktorePytanie).isOdpowiedz() != udzielonaOdpowniedz){
+            pytania.get(ktorePytanie).setCzyOdpowiedzOk(true);
+            Toast.makeText(this, "Udzielono poprawną odpowiedź", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            pytania.get(ktorePytanie).setCzyOdpowiedzOk(false);
+            Toast.makeText(this, "Udzielono złą odpowiedź", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
--- app/src/main/java/com/example/quiz/Pytanie.java
+++ app/src/main/java/com/example/quiz/Pytanie.java
@@ -1,4 +1,39 @@
 package com.example.quiz;
 
 public class Pytanie {
+    private int idObrazek;
+    private String trescPytania, podpowiedz;
+    private boolean odpowiedz, czyOdpowiedzOk;
+
+    public Pytanie(int idObrazek, String trescPytania, String podpowiedz, boolean odpowiedz) {
+        this.idObrazek = idObrazek;
+        this.trescPytania = trescPytania;
+        this.podpowiedz = podpowiedz;
+        this.odpowiedz = odpowiedz;
+        czyOdpowiedzOk = false;
+    }
+
+    public boolean isOdpowiedz() {
+        return odpowiedz;
+    }
+
+    public boolean isCzyOdpowiedzOk() {
+        return czyOdpowiedzOk;
+    }
+
+    public int getIdObrazek() {
+        return idObrazek;
+    }
+
+    public String getTrescPytania() {
+        return trescPytania;
+    }
+
+    public String getPodpowiedz() {
+        return podpowiedz;
+    }
+
+    public void setCzyOdpowiedzOk(boolean czyOdpowiedzOk) {
+        this.czyOdpowiedzOk = czyOdpowiedzOk;
+    }
 }
